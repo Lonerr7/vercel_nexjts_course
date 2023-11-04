edd1e870b7340779ef31b77053fd8be6edff3920
Lonerr7 <krabik2342@gmail.com>
2023-11-04 10:29:57 +0300

    4.2. Added debounce to the search and created pagination

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -4,6 +4,8 @@ import Search from '@/app/ui/search';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { FC, Suspense } from 'react';
 import Table from '@/app/ui/invoices/table';
+import Pagination from '@/app/ui/invoices/pagination';
+import { fetchInvoicesPages } from '@/app/lib/data';
 
 interface Props {
   searchParams?: {
@@ -12,9 +14,10 @@ interface Props {
   };
 }
 
-const Invoices: FC<Props> = ({ searchParams }) => {
+const Invoices: FC<Props> = async ({ searchParams }) => {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
 
   return (
     <div className="w-full">
@@ -29,7 +32,7 @@ const Invoices: FC<Props> = ({ searchParams }) => {
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
